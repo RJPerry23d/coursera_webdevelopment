deaf433d667794f162f34cfc049d069c99a5f093
RJ Perry <rj@blacklambs.com>
2020-09-23 16:14:37 -0400

    added solution correction to SpeakGoodBye.js file

--- module4/module4-solution/SpeakGoodBye.js
+++ module4/module4-solution/SpeakGoodBye.js
@@ -20,7 +20,7 @@
   // byeSpeaker object instead of being a standalone function.
   // See Lecture 52, part 2
   byeSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);;
+    console.log(speakWord + " " + name);
   }
 
 
